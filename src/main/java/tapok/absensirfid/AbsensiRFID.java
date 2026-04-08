96b094c10413f176d0ab7c0a6547f7c8b1c38ef0
pashalahh <ramdinpasha@gmail.com>
2026-04-08 18:50:48 +0700

    First Commit

---
+++ src/main/java/tapok/absensirfid/AbsensiRFID.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package tapok.absensirfid;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class AbsensiRFID {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
